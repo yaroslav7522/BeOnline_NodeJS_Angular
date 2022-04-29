20076413e38864b62ba8393f186305e89ae96b4e
yaroslav7522 <yaroslav7522@users.noreply.github.com>
2022-04-29 22:07:35 +0300

    test 4

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
 const passport = require('passport')
-//var path = require('path')
 
 const app = express()
 mongoose.connect(keys.mongoUrl)
@@ -17,7 +16,6 @@ const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
-//const clientRoutes = require('./client/src/index.html')
 
 const angular = require('./routes/client')
 
@@ -36,12 +34,5 @@ app.use('/api/category', categoryRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/position', positionRoutes)
 app.use('/api/client', clientRoutes)
-
-//app.use('/client', angular)
-
-//Any routes will be redirected to the angular app
-//app.get('*', function(req, res) {
-//    res.sendFile(path.join(__dirname, 'client/src/index.html'));
-//});
 
 module.exports = app
