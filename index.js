8a29da566f191a2f5cdca7815d4cd91ee4388233
yaroslav7522 <yaroslav7522@users.noreply.github.com>
2022-05-03 22:54:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const port = 8080 //process.env.PORT || 
+const port = 8080 //process.env.PORT  || 
 
 
 app.listen(port, () => { console.log('Server has been started at port ' + port + '!'); })
