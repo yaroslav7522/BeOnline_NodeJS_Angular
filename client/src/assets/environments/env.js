e7efd893e22119516978a93d44ec2ebb4ebfb303
yaroslav7522 <yaroslav7522@users.noreply.github.com>
2022-05-11 23:19:13 +0300

    upd20220511

---
+++ client/src/assets/environments/env.js
@@ -0,0 +1,4 @@
+(function (window) {
+  window.__env = window.__env || {};
+  window.__env.SERVER_URL = 'http://localhost:5000';
+}(this));
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const app = require('./app')
 const port = process.env.PORT || 5000
 
+app.use(sendSpaFileIfUnmatched);
+function sendSpaFileIfUnmatched(req, res){
+    res.sendFile("/client/dist/client/index.html", { root: '.' });
+}
 
 app.listen(port, () => { console.log('Server has been started at port ' + port + '!'); })
